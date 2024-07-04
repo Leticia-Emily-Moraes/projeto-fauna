72b1687434e09acc596e904e31b23b6079e9e03f
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-07-03 22:43:06 -0300

    feat: Adicionado função de verificação de numeros

--- FaunABC/src/components/inputText/index.js
+++ FaunABC/src/components/inputText/index.js
@@ -1,18 +1,37 @@
-import React from 'react';
-import { Container, Input, Label } from './style';
+import React, { useState } from 'react';
+import { Container, Input, Label, TextError } from './style';
 
-function Input({ TituloDoInput = 'Título', placeholder = 'Digite aqui...', value = '', onChangeText = () => {}, ...props }) {
+function InputText({ TituloDoInput = 'Título', placeholder = 'Digite aqui...', value = '', onChangeText = () => {}, ...props }) {
+  const [text, setText] = useState(text);
+  const [isValid, setIsValid] = useState(true);
+
+  const validateTextInput = (text) => {
+    if (text.length > 0) {
+      const numberPattern = /[0-9]/;
+      return !numberPattern.test(text)
+    }else{
+      return false
+    }
+  }
+
+  const handleTextChange = (text) => {
+    setText(text)
+    setIsValid(validateTextInput(text))
+    onChangeText(text)
+  }
+
   return (
     <Container>
       <Label>{TituloDoInput}</Label>
       <Input
         placeholder={placeholder}
-        value={value}
-        onChangeText={onChangeText}
+        value={text}
+        onChangeText={handleTextChange}
         {...props}
       />
+      {!isValid && <TextError>Não pode conter números</TextError>}
     </Container>
   );
 }
 
-export default Input;
+export default InputText;
