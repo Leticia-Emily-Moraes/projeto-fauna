bbf41eac808354a52141fea5f7abc62f55562b09
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-07-16 13:58:15 -0300

    feat: Adicionado page de config de rotas

---
+++ FaunABC/src/routes/stack.routes.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import PrimeiraPagina from "../pages/primeiraPagina";
+import CadastroUser from "../pages/cadastroUser";
+
+const Stack = createNativeStackNavigator();
+
+export function StackRoutes() {
+    return (
+        <Stack.Navigator
+            initialRouteName="primeira pagina"
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <Stack.Screen
+                name="primeira pagina"
+                component={PrimeiraPagina}
+            />
+            <Stack.Screen
+                name="cadastro user"
+                component={CadastroUser}
+            />
+        </Stack.Navigator>
+    );
+}
