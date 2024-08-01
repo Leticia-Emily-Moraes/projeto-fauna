8736bc8277dfe1c5af3b16daafc12a8f356e8579
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-08-01 19:17:31 -0300

    fix: arrumando as aspas

--- src/service/verificaCEP.js
+++ src/service/verificaCEP.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 const getAddress = async (cep) => {
-    try {
-        const response = await axios.get(
-            `https://viacep.com.br/ws/${cep}/json/`
-        );
-        if (response.data.erro) {
-            throw new Error("CEP não encontrado");
-        }
-        return response.data;
-    } catch(error){
-        throw new Error('Erro ao buscar o CEP');
-    }
+	try {
+		const response = await axios.get(
+			`https://viacep.com.br/ws/${cep}/json/`,
+		);
+		if (response.data.erro) {
+			throw new Error("CEP não encontrado");
+		}
+		return response.data;
+	} catch (error) {
+		throw new Error("Erro ao buscar o CEP");
+	}
 };
 
 export default getAddress;
